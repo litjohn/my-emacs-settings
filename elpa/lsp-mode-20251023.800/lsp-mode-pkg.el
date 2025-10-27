f9c5065f5addabf653642897cc749bbd0a137cf8
litjohn <rong7ming@gmail.com>
2025-10-27 20:49:19 +0800

    add elpa

---
+++ elpa/lsp-mode-20251023.800/lsp-mode-pkg.el
@@ -0,0 +1,15 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "lsp-mode" "20251023.800"
+  "LSP mode."
+  '((emacs         "28.1")
+    (dash          "2.18.0")
+    (f             "0.20.0")
+    (ht            "2.3")
+    (spinner       "1.7.3")
+    (markdown-mode "2.3")
+    (lv            "0")
+    (eldoc         "1.11"))
+  :url "https://github.com/emacs-lsp/lsp-mode"
+  :commit "8cf5a2e4e5510aec45b54b641157cbc24e1a6c9b"
+  :revdesc "8cf5a2e4e551"
+  :keywords '("languages"))
