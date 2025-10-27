2ab4d58131291847d5082ac7f97f72358e1286bc
litjohn <rong7ming@gmail.com>
2025-10-27 20:56:47 +0800

    Stop tracking elpa directory

--- elpa/zenburn-theme-20251012.1810/zenburn-theme-pkg.el
+++
@@ -1,9 +0,0 @@
-;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "zenburn-theme" "20251012.1810"
-  "A low contrast color theme for Emacs."
-  ()
-  :url "http://github.com/bbatsov/zenburn-emacs"
-  :commit "b1ef84f7c7102e1d26be67edc6a7ce9698e95f5e"
-  :revdesc "b1ef84f7c710"
-  :authors '(("Bozhidar Batsov" . "bozhidar@batsov.com"))
-  :maintainers '(("Bozhidar Batsov" . "bozhidar@batsov.com")))
--- elpa/zop-to-char-20160212.1554/zop-to-char-pkg.el
+++
@@ -1,9 +0,0 @@
-;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "zop-to-char" "20160212.1554"
-  "A replacement of zap-to-char."
-  '((cl-lib "0.5"))
-  :url "https://github.com/thierryvolpiatto/zop-to-char"
-  :commit "816ea90337db0545a2f0a5079f4d7b3a2822af7d"
-  :revdesc "816ea90337db"
-  :authors '(("Thierry Volpiatto" . "thierry.volpiatto@gmail.com"))
-  :maintainers '(("Thierry Volpiatto" . "thierry.volpiatto@gmail.com")))
